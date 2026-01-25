4a7bacc926030ae85a1ae30b15ee4965b50c70f0
bottleEveningTech <tanishkasaxena2408@gmail.com>
2026-01-25 17:48:41 +0530

    using async pipe

--- src/app/messages/messages-list/messages-list.component.ts
+++ src/app/messages/messages-list/messages-list.component.ts
@@ -1,21 +1,24 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, input, OnInit } from '@angular/core';
 import { MessagesService } from '../messages.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-messages-list',
   standalone: true,
+  imports: [AsyncPipe],
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MessagesListComponent implements OnInit{
+export class MessagesListComponent{
   // messages = input.required<string[]>();
 
   private messageService = inject(MessagesService);
+  messageList$ = this.messageService.messsages$;
   // messages = this.messageService.allMessages;
 
-  private cdRef = inject(ChangeDetectorRef);
-  private destroyRef = inject(DestroyRef);
+  // private cdRef = inject(ChangeDetectorRef);
+  // private destroyRef = inject(DestroyRef);
   // get messages(){
   //   return this.messageService.allMessages;
   // }
@@ -25,13 +28,13 @@ export class MessagesListComponent implements OnInit{
     return 'MessagesList Component Debug Output';
   }
 
-  ngOnInit(): void {
-    const messageSubscription = this.messageService.messsages$.subscribe((messages)=> {
-      this.messages = messages;
-      this.cdRef.markForCheck();
-    });
-    this.destroyRef.onDestroy(()=> {
-      messageSubscription.unsubscribe();
-    });
-  }
+  // ngOnInit(): void {
+  //   const messageSubscription = this.messageService.messsages$.subscribe((messages)=> {
+  //     this.messages = messages;
+  //     this.cdRef.markForCheck();
+  //   });
+  //   this.destroyRef.onDestroy(()=> {
+  //     messageSubscription.unsubscribe();
+  //   });
+  // }
 }
