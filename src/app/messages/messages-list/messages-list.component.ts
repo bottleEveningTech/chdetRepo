f4858fd29726b7c8106646caf1f8d32247fe8aa4
bottleEveningTech <tanishkasaxena2408@gmail.com>
2026-01-25 17:13:00 +0530

    Using behaviourSubject and setting subscription

--- src/app/messages/messages-list/messages-list.component.ts
+++ src/app/messages/messages-list/messages-list.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, input, OnInit } from '@angular/core';
 import { MessagesService } from '../messages.service';
 
 @Component({
@@ -8,17 +8,30 @@ import { MessagesService } from '../messages.service';
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MessagesListComponent {
+export class MessagesListComponent implements OnInit{
   // messages = input.required<string[]>();
 
   private messageService = inject(MessagesService);
   // messages = this.messageService.allMessages;
 
-  get messages(){
-    return this.messageService.allMessages;
-  }
+  private cdRef = inject(ChangeDetectorRef);
+  private destroyRef = inject(DestroyRef);
+  // get messages(){
+  //   return this.messageService.allMessages;
+  // }
+  messages:string[] = [];
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
     return 'MessagesList Component Debug Output';
+  }
+
+  ngOnInit(): void {
+    const messageSubscription = this.messageService.messsages$.subscribe((messages)=> {
+      this.messages = messages;
+      this.cdRef.markForCheck();
+    });
+    this.destroyRef.onDestroy(()=> {
+      messageSubscription.unsubscribe();
+    });
   }
 }
--- src/app/messages/messages.service.ts
+++ src/app/messages/messages.service.ts
@@ -1,18 +1,22 @@
 import { Injectable, signal } from '@angular/core';
-
+import { BehaviorSubject } from 'rxjs'
 @Injectable({
   providedIn: 'root',
 })
 export class MessagesService {
+
+  messsages$ = new BehaviorSubject<string[]>([]);
+
   // private messages = signal<string[]>([]);
   private messages:string[] = [];
 
   // allMessages = this.messages.asReadonly();
   get allMessages(){
-    return this.messages;
+    return [...this.messages];
   }
   addMessage(message: string) {
     // this.messages.update((prevMessages) => [...prevMessages, message]);
     this.messages = [...this.messages, message];
+    this.messsages$.next([... this.messages]);
   }
 }
